c34b4b847af7a9e23420f97eb3140c8888adc8a8
FAHTONI <ahmadfahtoni21@gmail.com>
2025-10-27 12:04:05 +0700

    Create HomePage component with layout sections

---
+++ HomePage.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Navbar from '../components/Navbar';
+import HeroSection from '../components/HeroSection';
+import FeaturesSection from '../components/FeaturesSection';
+import HowItWorksSection from '../components/HowItWorksSection';
+import Footer from '../components/Footer';
+
+const HomePage = () => {
+  return (
+    <div className="min-h-screen">
+      <Navbar />
+      <HeroSection />
+      <FeaturesSection />
+      <HowItWorksSection />
+      <Footer />
+    </div>
+  );
+};
+
+export default HomePage;
