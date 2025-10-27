f63138639a56837bd0f21d17b40ed9c86963537a
FAHTONI <ahmadfahtoni21@gmail.com>
2025-10-27 12:06:50 +0700

    Add Footer component with copyright information

---
+++ Footer.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="bg-gray-900 text-gray-300 py-8">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="text-center">
+          <p className="text-sm">
+            © 2025 PKL Management System. All rights reserved.
+          </p>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
